6353dea6e75c37e4e93cfad18fb5f3641f40db90
JzzzzSkr <huahuiie@outlook.com>
2024-08-08 16:47:44 +1000

    全球样式完成

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './page.scss';
 
 const Page = () => {
     return (
-        <div>
-            I am home 
+        <div className='home-wrpper'>
+            I am home
         </div>
     );
 }
