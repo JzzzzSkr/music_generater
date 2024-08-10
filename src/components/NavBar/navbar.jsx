1723196b4286524bc3d9374a62f176a803c9a64c
JzzzzSkr <huahuiie@outlook.com>
2024-08-10 18:24:41 +1000

    导航栏左侧githup图标完成

--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import Link from "next/link";
 import "./navbar.scss";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 const Navbar = () => {
-  return <div className="navbar-wrappter">I am navBar</div>;
+  return (
+    <div className="navbar-wrappter">
+      <a href="https://github.com/JzzzzSkr" className="left">
+        <div className="logo">
+          <GitHubIcon className="icon"></GitHubIcon>
+          <span className="name">JzzzzSkr</span>
+        </div>
+      </a>
+      <div className="middle">2</div>
+      <div className="right">3</div>
+    </div>
+  );
 };
 
 export default Navbar;
