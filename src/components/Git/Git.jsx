43abb2eeaeab6058f644abd09f390cd6955d12e0
JzzzzSkr <huahuiie@outlook.com>
2024-08-12 15:58:57 +1000

    done

--- src/components/Git/Git.jsx
+++ src/components/Git/Git.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import './Git.scss';
+import React from "react";
+import "./Git.scss";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 const Git = () => {
-    return (
-        <div className='git-wrapper'>
-            I am git 
+  return (
+    <div className="git-wrapper">
+      <h3 className="title">This project is Proudly Open Source</h3>
+      <p className="subtitle">The source code is available on GitHup.</p>
+      <a className="toGitButton">
+        <div className="left">
+          <GitHubIcon className="Git-icon"></GitHubIcon>
         </div>
-    );
-}
+        <span>Start on GitHup</span>
+      </a>
+    </div>
+  );
+};
 
 export default Git;
