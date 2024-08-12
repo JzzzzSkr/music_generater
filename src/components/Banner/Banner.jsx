3efeca4ab4720113d029fbfcfea9b6f1441a539a
JzzzzSkr <huahuiie@outlook.com>
2024-08-12 15:28:17 +1000

    banner done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import './Banner.scss';
+import React from "react";
+import "./Banner.scss";
+import { Button } from "@mui/material";
 
 const Banner = () => {
-    return (
-        <div className='banner-wrapper'>
-            I am Banner
-        </div>
-    );
-}
+  return (
+    <div className="banner-wrapper">
+      <div className="up">
+        <h3>Generate your favourite music in seconds</h3>
+      </div>
+      <div className="middle">
+        <p>
+          This web page makes it easy for you to generate music in seconds,
+          completely for free.
+        </p>
+      </div>
+      <div className="done">
+        <Button>Generate your music</Button>
+        <Button>Learn more</Button>
+      </div>
+    </div>
+  );
+};
 
 export default Banner;
